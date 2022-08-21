38071c08ee86925fc1da1b591ddbd49f068c1d7d
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-21 12:51:46 +0300

    socket module created

---
+++ src/main/java/com/gucardev/mp3playerbe/socket/SocketModule.java
@@ -0,0 +1,52 @@
+package com.gucardev.mp3playerbe.socket;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+import com.gucardev.mp3playerbe.model.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SocketModule {
+
+
+    private final SocketIOServer server;
+
+    public SocketModule(SocketIOServer server) {
+        this.server = server;
+        server.addConnectListener(onConnected());
+        server.addDisconnectListener(onDisconnected());
+        server.addEventListener("commands", Message.class, onChatReceived());
+
+    }
+
+
+    private DataListener<Message> onChatReceived() {
+        return (senderClient, data, ackSender) -> {
+            data.getCommand().execute(data.getRoom(), senderClient);
+        };
+    }
+
+
+    private ConnectListener onConnected() {
+        return (client) -> {
+
+            String room = client.getHandshakeData().getSingleUrlParam("room");
+            client.joinRoom(room);
+            log.info("Socket ID[{}] - room[{}]  Connected to chat module through", client.getSessionId().toString(), room);
+        };
+
+    }
+
+    private DisconnectListener onDisconnected() {
+        return client -> {
+            log.info("Client[{}] - Disconnected from chat module.", client.getSessionId().toString());
+        };
+    }
+
+
+}
