dac768afbe6d24d6217bd14a926a084e4d67e6bf
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-22 00:22:12 +0300

    get music list endpoint added

--- src/main/java/com/gucardev/mp3playerbe/controller/MP3Controller.java
+++ src/main/java/com/gucardev/mp3playerbe/controller/MP3Controller.java
@@ -1,11 +1,15 @@
 package com.gucardev.mp3playerbe.controller;
 
+import com.gucardev.mp3playerbe.model.FileModel;
+import com.gucardev.mp3playerbe.service.FileService;
 import com.gucardev.mp3playerbe.service.MP3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/mp3")
@@ -14,13 +18,26 @@ public class MP3Controller {
 
 
     private final MP3Service mp3Service;
+    private final FileService fileService;
 
-
-    @GetMapping(value = "/music/{id}.mp3" , produces = "audio/mp3")
-    public Mono<Resource> getVideo(@PathVariable Long id) {
-        return mp3Service.getMusicStreaming(id);
+    @CrossOrigin
+    @GetMapping(value = "/music")
+    public ResponseEntity<List<FileModel>> getMusicList() {
+        return ResponseEntity.ok(fileService.getFiles());
     }
 
+    @CrossOrigin
+    @GetMapping(value = "/music/{id}")
+    public ResponseEntity<FileModel> getMusicList(@PathVariable Long id) {
+        return ResponseEntity.ok(fileService.getById(id));
+    }
+
+
+    @CrossOrigin
+    @GetMapping(value = "/music/{id}.mp3", produces = "audio/mp3")
+    public Mono<Resource> getMusic(@PathVariable Long id) {
+        return mp3Service.getMusicStreaming(id);
+    }
 
 
 }
