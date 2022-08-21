ebc1b71376d281b1fc7b428c93fdcdbd65088922
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-21 15:19:53 +0300

    command handler service created

---
+++ src/main/java/com/gucardev/mp3playerbe/socket/CommandHandlerService.java
@@ -0,0 +1,28 @@
+package com.gucardev.mp3playerbe.socket;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import com.gucardev.mp3playerbe.command.Command;
+import com.gucardev.mp3playerbe.command.CommandType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+public class CommandHandlerService {
+
+    @Autowired
+    private Map<String, Command> commandHandlersMap;
+
+    public void executeCommand(CommandType commandType,
+                               String room,
+                               SocketIOClient senderClient,
+                               String commandValue) {
+        log.info("Selected command:" + commandType.beanName());
+        commandHandlersMap.get(commandType.beanName()).execute(room, commandValue, senderClient);
+    }
+
+
+}
