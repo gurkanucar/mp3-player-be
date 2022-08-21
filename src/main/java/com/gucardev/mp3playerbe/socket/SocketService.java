7c757a875ff0789c4d518383a542c24e170a9155
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-21 15:24:01 +0300

    socket service updated

--- src/main/java/com/gucardev/mp3playerbe/socket/SocketService.java
+++ src/main/java/com/gucardev/mp3playerbe/socket/SocketService.java
@@ -1,14 +1,22 @@
 package com.gucardev.mp3playerbe.socket;
 
 import com.corundumstudio.socketio.SocketIOClient;
-import com.gucardev.mp3playerbe.command.CommandNameConstants;
+import com.gucardev.mp3playerbe.command.Command;
+import com.gucardev.mp3playerbe.command.CommandType;
 import com.gucardev.mp3playerbe.command.CommandResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
+@Slf4j
 public class SocketService {
 
-    public void sendCommand(String room, SocketIOClient senderClient, CommandNameConstants commandName, String commandValue) {
+
+    public void sendCommand(String room, SocketIOClient senderClient, CommandType commandName, String commandValue) {
+        log.info("Command sent: " + commandName.toString());
         for (
                 SocketIOClient client : senderClient.getNamespace().getRoomOperations(room).getClients()) {
             if (!client.getSessionId().equals(senderClient.getSessionId())) {
