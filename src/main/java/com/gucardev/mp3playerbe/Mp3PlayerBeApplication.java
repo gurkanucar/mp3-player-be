ea0a698fb98e62ba32d766e663fe638818631172
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-21 10:53:42 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/mp3playerbe/Mp3PlayerBeApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.mp3playerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3PlayerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mp3PlayerBeApplication.class, args);
+    }
+
+}
